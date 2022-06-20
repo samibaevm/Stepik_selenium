40a1a3f9ab1eb94969a5b2f2094b5986282b1ecd
samibaevm <samibaevm@users.noreply.github.com>
2022-06-21 00:06:37 +0300

    Began learning selenium

--- 2.3.6.py
+++ 2.3.6.py
@@ -3,20 +3,20 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
-
 def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 
 try:
     browser = webdriver.Chrome()
-    browser.get('http://suninjuly.github.io/alert_accept.html')
-    browser.find_element(By.CSS_SELECTOR, 'body > form > div > div > button').click()
-    browser.switch_to.alert.accept()
-    x = browser.find_element(By.CSS_SELECTOR, '#input_value').text
-    result = calc(x)
+    browser.get('http://suninjuly.github.io/redirect_accept.html')
+    browser.find_element(By.XPATH, '/html/body/form/div/div/button').click()
+    new_window = browser.window_handles[1]
+    old_wndow = browser.window_handles[0]
+    browser.switch_to.window(new_window)
+    value = browser.find_element(By.CSS_SELECTOR, '#input_value').text
+    result = calc(value)
     browser.find_element(By.CSS_SELECTOR, '#answer').send_keys(result)
     browser.find_element(By.CSS_SELECTOR, 'body > form > div > div > button').click()
-
 
 
 finally:
